47296ba4ccc9c767de8cd955adb5bde95ad921c7
rupankarghosh <rupankar326@gmail.com>
2020-02-24 16:07:25 +0530

    creating the first main code snippet with only comments

---
+++ gnome.py
@@ -0,0 +1,15 @@
+#/bin/env python3
+
+'''this python tool will purge data from
+remote database and store the results in
+thr local dedicated directory
+'''
+
+# importing modules
+
+import requests
+import json
+import os
+
+# the main codehub codes
+
